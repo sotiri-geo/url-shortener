838b641ae1575be1ca40db32b69d125bde671cf6
Sotiri Georgiou <tirigeorgiou@gmail.com>
2025-09-08 18:10:22 +0100

    test: post /shorten

--- server.go
+++ server.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 )
 
+type URLShortResponse struct {
+	Short string
+}
+
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "ok")
+}
+
+func URLServer(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(URLShortResponse{Short: "abc123"})
 }
--- server_test.go
+++ server_test.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
 // type HandlerFunc func(ResponseWriter, *Request)
 
 func TestHealthCheckEndpoint(t *testing.T) {
-	req, _ := http.NewRequest(http.MethodGet, "/health", nil)
+	req := httptest.NewRequest(http.MethodGet, "/health", nil)
 	response := httptest.NewRecorder()
 
 	HealthCheck(response, req)
@@ -18,7 +20,43 @@ func TestHealthCheckEndpoint(t *testing.T) {
 
 	want := "ok"
 
+	if response.Code != http.StatusOK {
+		t.Fatalf("got status %d, want %d", response.Code, http.StatusOK)
+	}
+
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestShortenURL(t *testing.T) {
+	t.Run("POST /shorten returns a shortened url", func(t *testing.T) {
+		body := `{ "url": "https://example.com" }`
+		req := httptest.NewRequest(http.MethodPost, "/shorten", strings.NewReader(body))
+
+		response := httptest.NewRecorder()
+		want := "abc123"
+		URLServer(response, req)
+		var got URLShortResponse
+
+		// decode
+		err := json.NewDecoder(response.Body).Decode(&got)
+
+		if err != nil {
+			t.Fatalf("failed to decode body: %v", err)
+		}
+
+		assertStatusCode(t, response.Code, http.StatusCreated)
+		if got.Short != want {
+			t.Errorf("got %q, want %q", got.Short, want)
+		}
+	})
+}
+
+func assertStatusCode(t testing.TB, got, want int) {
+	t.Helper()
+
+	if got != want {
+		t.Fatalf("incorrect status code: got %d, want %d", got, want)
 	}
 }
