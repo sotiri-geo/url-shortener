927a9e0dfd3e6681a85c95196837660dc3e8d314
Sotiri Georgiou <tirigeorgiou@gmail.com>
2025-09-08 19:36:13 +0100

    fix: temp inmemory store

--- main.go
+++ main.go
@@ -5,10 +5,18 @@ import (
 	"net/http"
 )
 
+// hard coded implementation of store for now
+
+type InMemoryURLStore struct{}
+
+func (i *InMemoryURLStore) GetShortURL(url string) string {
+	return "abc123"
+}
+
 func main() {
 
 	// Create handle func and register route
 	http.HandleFunc("/health", HealthCheck)
-
+	http.Handle("/shortener", &URLServer{&InMemoryURLStore{}})
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
