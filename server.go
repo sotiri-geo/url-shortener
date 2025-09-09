6d56622e706d908d1551aa425ca13066742c89eb
Sotiri Georgiou <tirigeorgiou@gmail.com>
2025-09-09 09:05:21 +0100

    refactor: err response passed in as dep injection when writing err response

--- server.go
+++ server.go
@@ -65,11 +65,13 @@ func (u *URLServer) processURL(w http.ResponseWriter, r *http.Request) {
 	err := json.NewDecoder(r.Body).Decode(&req)
 
 	if err != nil {
-		u.writeErrorResponse(w, http.StatusBadRequest, ERR_INVALID_JSON, ERR_INVALID_JSON_CODE, ERR_INVALID_JSON_DETAILS)
+		errResponse := NewErrorResponse(ERR_INVALID_JSON, ERR_INVALID_JSON_CODE, ERR_INVALID_JSON_DETAILS)
+		u.writeErrorResponse(w, http.StatusBadRequest, errResponse)
 		return
 	}
 	if req.URL == "" {
-		u.writeErrorResponse(w, http.StatusBadRequest, ERR_EMPTY_URL, ERR_EMPTY_URL_CODE, ERR_EMPTY_URL_DETAILS)
+		errResponse := NewErrorResponse(ERR_EMPTY_URL, ERR_EMPTY_URL_CODE, ERR_EMPTY_URL_DETAILS)
+		u.writeErrorResponse(w, http.StatusBadRequest, errResponse)
 		return
 	}
 
@@ -81,13 +83,18 @@ func (u *URLServer) processURL(w http.ResponseWriter, r *http.Request) {
 func (u *URLServer) redirectURL(w http.ResponseWriter, r *http.Request) {
 	shortCode, exists := u.store.GetOriginalURL(path.Base(r.URL.Path))
 	if !exists {
-		u.writeErrorResponse(w, http.StatusNotFound, ERR_SHORT_CODE_NOT_FOUND, ERR_SHORT_CODE_NOT_FOUND_CODE, ERR_SHORT_CODE_NOT_FOUND_DETAILS)
+		errResponse := NewErrorResponse(ERR_SHORT_CODE_NOT_FOUND, ERR_SHORT_CODE_NOT_FOUND_CODE, ERR_SHORT_CODE_NOT_FOUND_DETAILS)
+		u.writeErrorResponse(w, http.StatusNotFound, errResponse)
 	}
 	w.WriteHeader(http.StatusFound)
 	http.Redirect(w, r, shortCode, http.StatusFound)
 }
 
-func (u *URLServer) writeErrorResponse(w http.ResponseWriter, status int, message, code, details string) {
+func (u *URLServer) writeErrorResponse(w http.ResponseWriter, status int, errorResponse *ErrorResponse) {
 	w.WriteHeader(status)
-	json.NewEncoder(w).Encode(ErrorResponse{message, code, details})
+	json.NewEncoder(w).Encode(errorResponse)
+}
+
+func NewErrorResponse(message, code, details string) *ErrorResponse {
+	return &ErrorResponse{message, code, details}
 }
