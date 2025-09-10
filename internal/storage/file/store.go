aaf51a12437c3946f864ce6eab4b3641a1d4c9f3
Sotiri Georgiou <tirigeorgiou@gmail.com>
2025-09-10 20:32:52 +0100

    refactor: read through cache design

--- internal/storage/file/store.go
+++ internal/storage/file/store.go
@@ -10,30 +10,31 @@ import (
 
 type FileStore struct {
 	Database io.ReadWriteSeeker
+	urls     handler.URL
 }
 
 func (f *FileStore) Exists(shortCode string) (bool, error) {
-	urls, err := f.load()
+	err := f.loadFromCache()
 	if err != nil {
 		return false, fmt.Errorf("failed to execute method Exists(%q): %v", shortCode, err)
 	}
-	_, exists := urls[shortCode]
+	_, exists := f.urls[shortCode]
 	return exists, nil
 }
 
 func (f *FileStore) GetOriginalURL(shortCode string) (string, error) {
-	urls, err := f.load()
+	err := f.loadFromCache()
 	if err != nil {
 		return "", fmt.Errorf("failed to get original url from short code %q: %v", shortCode, err)
 	}
 
-	return urls[shortCode], nil
+	return f.urls[shortCode], nil
 }
 
 func (f *FileStore) Save(shortCode, originalUrl string) error {
-	urls, err := f.load()
+	urls, err := f.loadFromDisk()
 	if err != nil {
-		return fmt.Errorf("failed to load before saving: %v", err)
+		return fmt.Errorf("failed to loadFromDisk before saving: %v", err)
 	}
 
 	urls[shortCode] = originalUrl
@@ -48,13 +49,26 @@ func (f *FileStore) Save(shortCode, originalUrl string) error {
 	return err
 }
 
-func (f *FileStore) load() (handler.URL, error) {
+func (f *FileStore) loadFromDisk() (handler.URL, error) {
 	f.Database.Seek(0, io.SeekStart)
 	urls, err := handler.NewUrls(f.Database)
 	if err != nil {
 		return nil, fmt.Errorf("failed to read urls: %v", err)
 	}
 	return urls, err
+}
+
+// Behaves like a read through cache
+func (f *FileStore) loadFromCache() error {
+	if f.urls == nil {
+		// force load from disk
+		urls, err := f.loadFromDisk()
+		if err != nil {
+			return err
+		}
+		f.urls = urls
+	}
+	return nil
 }
 
 func NewFileStore(database io.ReadWriteSeeker) *FileStore {
